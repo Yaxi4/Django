5ed73fe65de2d475f3f386dd29ddde0f8700c820
Яхия <104417808+Yaxi4@users.noreply.github.com>
2022-10-05 18:52:02 +0500

    Add files via upload

---
+++ создание Джанго сервера.py
@@ -0,0 +1,11 @@
+python -m venv venv #виртуальное окружение
+python3 -m venv venv
+venv\scripts\activate #активация
+pip install django #установка Джанго
+django-admin startproject project #создание проекта
+cd project
+python manage.py runserver #запуск сервера
+http://127.0.0.1:8000/ #адрес сервера
+
+
+
